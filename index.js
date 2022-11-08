d09951e7ca581a1b5d3caf420d62ab920e8f7fe0
skhasancse18344 <kamrul.hasan.cse18344@gmail.com>
2022-11-09 02:39:06 +0600

    Call Data By A single Data id

--- index.js
+++ index.js
@@ -17,6 +17,34 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+async function run() {
+  try {
+    const serviceCollection = client.db("fatChef").collection("services");
+
+    app.get("/servicesFeature", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query).limit(3);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+    app.get("/items", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    app.get("/items/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+  } finally {
+  }
+}
+run().catch((err) => console.error(err));
+
 app.get("/", (req, res) => {
   res.send("Services Review Server is Running");
 });
