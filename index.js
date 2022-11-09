14d2d9281f4d014e98514913b1aa04965d72cd03
skhasancse18344 <kamrul.hasan.cse18344@gmail.com>
2022-11-09 15:42:17 +0600

    get data for reviews by id

--- index.js
+++ index.js
@@ -50,6 +50,17 @@ async function run() {
     });
 
     //Reviews API
+    app.get("/reviews", async (req, res) => {
+      let query = {};
+      if (req.query.service) {
+        query = {
+          service: req.query.service,
+        };
+      }
+      const cursor = reviewCollection.find(query);
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
     app.post("/reviews", async (req, res) => {
       const review = req.body;
       const result = await reviewCollection.insertOne(review);
