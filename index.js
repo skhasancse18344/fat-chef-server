f8393478c87bc31b70db2ae4b536ff39c1acfdca
skhasancse18344 <kamrul.hasan.cse18344@gmail.com>
2022-11-10 22:21:21 +0600

    Final Push

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ async function run() {
     app.get("/items/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
-      const service = await serviceCollection.findOne(query).sort({ time: -1 });
+      const service = await serviceCollection.findOne(query);
       res.send(service);
     });
     app.get("/addReview/:id", async (req, res) => {
